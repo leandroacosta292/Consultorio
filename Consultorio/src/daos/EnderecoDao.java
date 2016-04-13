631a518d2e33c0115674561ef08ba910bb7e8cb6
Leandro <Leandro@DESKTOP-43SGN2S>
2016-04-13 08:22:56 -0300

    enddao full

--- Consultorio/src/daos/EnderecoDao.java
+++ Consultorio/src/daos/EnderecoDao.java
@@ -5,8 +5,10 @@
  */
 package daos;
 
+import apoio.ConexaoBD;
 import entidades.Endereco;
 import interfaces.IDAO;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -14,29 +16,33 @@ import java.util.ArrayList;
  *
  * @author lacosta
  */
-public class EnderecoDao implements IDAO{
+public class EnderecoDao implements IDAO {
 
     @Override
     public String salvar(Object o) {
         Endereco endereco = (Endereco) o;
         try {
-           // Statement st = consultorio.Consultorio.conexao.createStatement();
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
 
-            String sql = "INSERT INTO categoria VALUES"
+            String sql = "INSERT INTO endereco VALUES"
                     + "(DEFAULT, "
-                    + "'" + endereco.getLogradouro() + "'"
-                    + "'" + endereco.getNumero()+ "'"
-                    + "'" + endereco.getComplemento() + "'"
-                    + "'" + endereco.getBairro()+ "'"
-                    + "'" + endereco.getCEP() + "'"
-                    + "'" + endereco.getCidade_Id()+ "')";
-            
+                    + "'" + endereco.getLogradouro() + "',"
+                    + "'" + endereco.getNumero() + "',"
+                    + "'" + endereco.getComplemento() + "',"
+                    + "'" + endereco.getBairro() + "',"
+                    + "'" + endereco.getCEP() + "',"
+                    + "'" + endereco.getCidade_Id() + "') RETURNING id_endereco";
+
             System.out.println("sql: " + sql);
 
-          //  int resultado = st.executeUpdate(sql);
-            return null;
+            ResultSet rs = st.executeQuery(sql);
+            int id = 0;
+            if (rs.next()) {
+                id = rs.getInt("id_endereco");
+            }
+            return String.valueOf(id);
         } catch (Exception e) {
-            System.out.println("Erro salvar endereco = " + e);
+            System.out.println("Erro salvar Endereco = " + e);
             return e.toString();
         }
     }
@@ -63,6 +69,33 @@ public class EnderecoDao implements IDAO{
 
     @Override
     public Object consultarId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * FROM endereco WHERE "
+                    + "id_endereco = " + id + "";
+
+           // System.out.println("sql: " + sql);
+
+            ResultSet resultado = st.executeQuery(sql);
+
+            if (resultado.next()) {
+                Endereco tmpEndereco = new Endereco();
+                tmpEndereco.setBairro(resultado.getString("bairro"));
+                tmpEndereco.setCEP(resultado.getString("cep"));
+                tmpEndereco.setComplemento(resultado.getString("complemento"));
+                tmpEndereco.setLogradouro(resultado.getString("logradouro"));
+                tmpEndereco.setNumero(resultado.getString("numero"));
+                tmpEndereco.setCidade_Id(resultado.getInt("cidade_id"));
+                return tmpEndereco;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Erro consultar pessoa = " + e);
+            return e.toString();
+        }
+
     }
 }
